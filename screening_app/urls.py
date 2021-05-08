8c2b3311806f56c239df3d9c5537f1f3d11343a5
BiRuVN <ngngquang99@gmail.com>
2021-05-08 14:58:13 +0700

    uncomment url get date

--- screening_app/urls.py
+++ screening_app/urls.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     # path('', ),
+    path('date/', get_date),
     path('room/', get_room),
     path('screening-room/', get_screening_by_room),
     path('screening-date/', get_screening_by_date),
