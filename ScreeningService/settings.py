3ce2da0527d24a9cd274275f6bd602657f64dc0b
BiRuVN <ngngquang99@gmail.com>
2021-05-13 13:34:01 +0700

    cors

--- ScreeningService/settings.py
+++ ScreeningService/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'ScreeningService.middleware.open_access_middleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
